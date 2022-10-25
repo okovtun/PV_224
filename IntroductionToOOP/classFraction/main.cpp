1287c3f6e739bad49654f0bfddd0a3ae4c9c7470
Clayman <Clayman@DESKTOP-DM8E77U>
2022-10-25 19:46:44 +0300

    Constructors overloaded in Frction

--- IntroductionToOOP/classFraction/main.cpp
+++ IntroductionToOOP/classFraction/main.cpp
@@ -32,10 +32,68 @@ public:
 		if (denominator == 0)denominator = 1;
 		this->denominator = denominator;
 	}
+
+	//					Constructors:
+	Fraction()
+	{
+		this->integer = 0;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	Fraction(int integer)
+	{
+		this->integer = integer;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "1ArgConstructor:" << this << endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		this->numerator = numerator;
+		set_denominator(denominator);
+		cout << "Constructor:\t" << this << endl;
+	}
+	Fraction(int integer, int numerator, int denominator)
+	{
+		this->integer = integer;
+		this->numerator = numerator;
+		set_denominator(denominator);
+		cout << "Constructor:\t" << this << endl;
+	}
+	~Fraction()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
 };
+
+ostream& operator<<(ostream& os, const Fraction& obj)
+{
+	if (obj.get_integer())os << obj.get_integer();
+	if (obj.get_numerator())
+	{
+		if (obj.get_integer())os << "(";
+		os << obj.get_numerator() << "/" << obj.get_denominator();
+		if (obj.get_integer())os << ")";
+	}
+	else if (obj.get_integer() == 0) os << 0;
+	return os;
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	Fraction A;		//Default constructor
+	cout << A << endl;
+	
+	double b = 5;
+	Fraction B = 5;	//Single-argument constructor
+	cout << B << endl;
+	
+	Fraction C(2, 3);
+	cout << C << endl;
 
+	Fraction D(2, 3, 4);
+	cout << D << endl;
 }
