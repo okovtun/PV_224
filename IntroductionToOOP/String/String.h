bdda3dc2601e6c5d32d002e236a8da775cdbcd0f
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-04 20:45:18 +0200

    Separation DONE

---
+++ IntroductionToOOP/String/String.h
@@ -0,0 +1,44 @@
+#pragma once
+//StringSeparation
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::size_t;
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////		ОБЪЯВЛЕНИЕ КЛАССА - CLASS DECLARATION			////////////
+
+class String;
+String operator+(const String& left, const String& right);
+ostream& operator<<(ostream& os, const String& obj);
+
+class String
+{
+	size_t size;	//размер строки в Байтах
+	char* str;		//указатель на строку в динамической памяти
+public:
+	const char* get_str()const;
+	char* get_str();
+	size_t get_size()const;
+	//				Constructors:
+	explicit String(size_t size = 80);
+	String(const char str[]);
+	String(const String& other);
+	String(String&& other);
+	~String();
+
+	//				Operators:
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	String& operator+=(const String& other);
+
+	char operator[](int i)const;
+	char& operator[](int i);
+
+	//				Methods:
+	void print()const;
+};
+/////////////	КОНЕЦ ОБЪЯВЛЕНИЯ КЛАССА - CLASS DECLARATION	END		/////////////
+////////////////////////////////////////////////////////////////////////////////
