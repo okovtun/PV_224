aca3a9b717c4032f75d1513e5be7b6f6cdd09d83
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-26 18:34:15 +0200

    ForwardList push_front() DONE

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -1,9 +1,79 @@
-//ForwardList
+﻿//ForwardList
 #include<iostream>
 using namespace std;
+
+#define tab "\t"
+
+class Element
+{
+	int Data;		//Значение элемента
+	Element* pNext;	//Адрес следующего элемента
+public:
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
+	{
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		cout << "EDestructor:\t" << this << endl;
+	}
+	friend class ForwardList;
+};
+
+class ForwardList
+{
+	Element* Head;	//Указатель на начальный элемент списка
+	//Голова является точкой входа в список
+public:
+	ForwardList() :Head(nullptr)
+	{
+		//Конструктор по умолчанию создает пустой список.
+		//Если Голова указывает на 0, то список пуст
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~ForwardList()
+	{
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//				Adding Elements:
+	void push_front(int Data)
+	{
+		//1) Создаем новый элемент:
+		Element* New = new Element(Data);
+		//2) Пристыковываем новый элемент к началу списка:
+		New->pNext = Head;
+		//3) Переводим Голову на новый элемент:
+		Head = New;
+	}
+	void push_back(int Data)
+	{
+		
+	}
+
+	//				Methods:
+	void print()const
+	{
+		Element* Temp = Head;	//Temp - это итератор.
+		//Итератор - это указатель, при помощи которого 
+		//можно получить доступ к элементам структуры данных.
+		while (Temp)
+		{
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+			Temp = Temp->pNext;	//Переход на следующий элемент
+		}
+	}
+};
 
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	ForwardList list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
 }
