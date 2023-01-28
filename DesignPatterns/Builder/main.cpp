2a80fcd782c818119c1e708ceb94283f77296c6b
Clayman <Clayman@DESKTOP-DM8E77U>
2023-01-29 01:10:13 +0300

    Builder DONE

--- DesignPatterns/Builder/main.cpp
+++ DesignPatterns/Builder/main.cpp
@@ -47,61 +47,181 @@ public:
 	Computer()
 	{
 		on = false;
+		cout << "CompConstructor:\t" << this << endl;
 	}
 	virtual ~Computer()
 	{
-		cout << "CDenstructor:\t" << this << endl;
+		cout << "CompDenstructor:\t" << this << endl;
+	}
+	void power_button()
+	{
+		on = on ? false : true;
 	}
 	virtual void info()const
 	{
-		cout << "CPU:" << cpu << ", ";
-		cout << "RAM:" << ram << ", ";
-		cout << "Video:" << video << ", ";
-		cout << "Disk:" << disk << ", ";
-		cout << "Power:" << power << ";\n";
+		cout << "\n--------------------------------------\n";
+		if (on)
+		{
+			cout << type << ":\t";
+			cout << "CPU:" << cpu << ", ";
+			cout << "RAM:" << ram << ", ";
+			cout << "Video:" << video << ", ";
+			cout << "Disk:" << disk << ", ";
+			cout << "Power:" << power << ";\n";
+		}
+		else
+		{
+			cout << "Please turn me on" << endl;
+		}
+		cout << "\n--------------------------------------\n";
 	}
 };
 
-class CheapComputer :public Computer
+//AbstractBuilder
+class ComputerBuilder
 {
+protected:
+	Computer* computer;
 public:
-	CheapComputer(const std::string& cpu, const std::string& ram, const std::string& video, const std::string& disk, const std::string& power)
-		:Computer(cpu, ram, video, disk, power)
+	ComputerBuilder()
 	{
-		cout << "CheapComputerConstructor:\t" << this << endl;
+		this->computer = new Computer;
 	}
-	~CheapComputer()
+	virtual ~ComputerBuilder()
 	{
-		cout << "CheapComputerDestructor:\t" << this << endl;
+		delete this->computer;
 	}
+	Computer* get_computer()const
+	{
+		return computer;
+	}
+	virtual void set_type() = 0;
+	virtual void set_cpu() = 0;
+	virtual void set_ram() = 0;
+	virtual void set_video() = 0;
+	virtual void set_disk() = 0;
+	virtual void set_power() = 0;
 };
-class OfficeComputer :public Computer
+
+//ConcreteBuilders:
+class CheapComputerBuilder :public ComputerBuilder
 {
 public:
-	OfficeComputer(const std::string& cpu, const std::string& ram, const std::string& video, const std::string& disk, const std::string& power)
-		:Computer(cpu, ram, video, disk, power)
+	~CheapComputerBuilder(){}
+	void set_type()
 	{
-		cout << "OfficeComputerConstructor:\t" << this << endl;
+		computer->set_type("CheapComputer");
 	}
-	~OfficeComputer()
+	void set_cpu()
 	{
-		cout << "OfficeComputerDestructor:\t" << this << endl;
+		computer->set_cpu("Intel Celeron 7300");
 	}
-};
-class GameComputer :public Computer
-{
-public:
-	GameComputer(const std::string& cpu, const std::string& ram, const std::string& video, const std::string& disk, const std::string& power)
-		:Computer(cpu, ram, video, disk, power)
+	void set_ram()
 	{
-		cout << "GameComputerConstructor:\t" << this << endl;
+		computer->set_ram("SiliconPower 4GB");
 	}
-	~GameComputer()
+	void set_video()
 	{
-		cout << "GameComputerDestructor:\t" << this << endl;
+		computer->set_video("Intel UHD 12G");
+	}
+	void set_disk()
+	{
+		computer->set_disk("Kingston SSD Now 128GB");
+	}
+	void set_power()
+	{
+		computer->set_power("Chieftec 400W");
 	}
 };
 
+class OfficeComputerBuilder :public ComputerBuilder
+{
+public:
+	~OfficeComputerBuilder() {}
+	void set_type()
+	{
+		computer->set_type("OfficeComputer");
+	}
+	void set_cpu()
+	{
+		computer->set_cpu("Intel Core-i3 13100");
+	}
+	void set_ram()
+	{
+		computer->set_ram("Kingston DDR4-3200 8GB");
+	}
+	void set_video()
+	{
+		computer->set_video("Intel UHD Graphics 730");
+	}
+	void set_disk()
+	{
+		computer->set_disk("Samsung 750 Evo 256GB");
+	}
+	void set_power()
+	{
+		computer->set_power("Chieftec 500W Silver");
+	}
+};
+
+class GameComputerBuilder :public ComputerBuilder
+{
+public:
+	~GameComputerBuilder() {}
+	void set_type()
+	{
+		computer->set_type("GameComputer");
+	}
+	void set_cpu()
+	{
+		computer->set_cpu("Intel Core-i7 13700");
+	}
+	void set_ram()
+	{
+		computer->set_ram("Kingston DDR5-5600 32GB");
+	}
+	void set_video()
+	{
+		computer->set_video("nVidia GeForce RTX-4090");
+	}
+	void set_disk()
+	{
+		computer->set_disk("2TB Samsung 850 Pro M.2");
+	}
+	void set_power()
+	{
+		computer->set_power("Seasonic 1000W Platinum");
+	}
+};
+
+class Sysadmin
+{
+	ComputerBuilder* builder;
+public:
+	Sysadmin() :builder(nullptr) {}
+	void assembly_computer(ComputerBuilder* builder)
+	{
+		this->builder = builder;
+		builder->set_type();
+		builder->set_cpu();
+		builder->set_ram();
+		builder->set_video();
+		builder->set_disk();
+		builder->set_power();
+	}
+	void turn_on_the_computer()
+	{
+		if (builder)
+		{
+			builder->get_computer()->power_button();
+			builder->get_computer()->info();
+		}
+		else
+		{
+			cout << "Средства на сборку компьютера не были выделены :-(" << endl;
+		}
+	}
+};
 
 void main()
 {
@@ -109,8 +229,18 @@ void main()
 	//Computer comp("Core i5 12400", "Kingston 8GB", "Intel HD Graphics", "Samsung 870 Evo 500 GB", "Chieftec 500 Gold");
 	//comp.info();
 
-	CheapComputer comp("Intel Celeron", "Kingston 4GB", "Intel HD Graphics", "Kingston SSD 128GB", "Chieftec 400");
-	comp.info();
+	Sysadmin nice_guy;
+	CheapComputerBuilder computer_for_director;
+	nice_guy.assembly_computer(&computer_for_director);
+	nice_guy.turn_on_the_computer();
+	computer_for_director.get_computer()->power_button();
+	computer_for_director.get_computer()->info();
 
+	OfficeComputerBuilder computer_for_bookkeeper;
+	nice_guy.assembly_computer(&computer_for_bookkeeper);
+	nice_guy.turn_on_the_computer();
 
+	GameComputerBuilder computer_for_sysadmin;
+	nice_guy.assembly_computer(&computer_for_sysadmin);
+	nice_guy.turn_on_the_computer();
 }
